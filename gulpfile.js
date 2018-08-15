058ed604155a97fb078525c99b3bc25af67d4081
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2018-08-15 10:52:21 -0500

    Pushing

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,22 @@ var gulp = require('gulp');
 var resume = require('gulp-resume');
 var rename = require('gulp-rename');
 
-gulp.task('resume', function() {
-  return gulp.src('resume.json')
+gulp.task('enrique', function() {
+  return gulp.src('enrique_resume.json')
     .pipe(resume({
       format: 'html',
       theme: 'elegant'
     }))
-    .pipe(rename('resume.html'))
+    .pipe(rename('enrique_resume.html'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('nicole', function() {
+  return gulp.src('nicole_resume.json')
+    .pipe(resume({
+      format: 'html',
+      theme: 'elegant'
+    }))
+    .pipe(rename('nicole_resume.html'))
     .pipe(gulp.dest('.'));
 });
